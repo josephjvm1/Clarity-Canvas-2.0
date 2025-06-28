4cca4190fbcf71acef85d8434fef9f283e264440
Joseph Mairena <joseph@palmtreeproperties.com>
2025-06-27 17:05:24 -0700

    Initial commit with Vite React app

---
+++ clarity-canvas/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Clarity Canvas</h1>
+      <p>This is your starting point. Let’s build something amazing.</p>
+    </div>
+  );
+}
+
+export default App;
